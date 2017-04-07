9ff47d3f9a6fede4d4b0266670e8f05511218910
macalityy <marcel.wittich@gmail.com>
2017-04-07 13:24:24 +0200

    Authentification Script

---
+++ authentification.R
@@ -0,0 +1,8 @@
+library("twitteR")
+library("ROAuth")
+api_keys <- read.csv2("api_key.csv", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+setup_twitter_oauth(
+  consumer_key = api_keys$consumer_key,
+  consumer_secret = api_keys$consumer_secret,
+  access_token = api_keys$access_token,
+  access_secret =  api_keys$access_token_secret )

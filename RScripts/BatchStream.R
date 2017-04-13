fc63cb36ef812e379af835ef3e8f4a5d9bd5aca1
macalityy <marcel.wittich@gmail.com>
2017-04-13 22:46:22 +0200

    Changed Script to work with multiple hashtags

--- RScripts/BatchStream.R
+++ RScripts/BatchStream.R
@@ -9,8 +9,11 @@ library("streamR")
 # Load authentification into workspace
 load("tw_oauth.Rdata")
 
+#get arguments from cmd line
 args <- commandArgs(trailingOnly = TRUE)
-tw_track <- args[1]
+#get hashtags and searchstrings from args
+tw_track <- unlist(strsplit(args[1], ", "))
+#get duration from args
 tw_duration <- as.numeric(args[2])
 
 #set filename
@@ -20,11 +23,11 @@ tw_filename <- paste(c("Data/", tw_track, "_", current.time, ".json"), collapse 
 #get twitter data
 filterStream(file.name = tw_filename, track = tw_track, timeout = tw_duration, oauth = tw_oauth)
 
-#parseData
-tweets <- parseTweets(tw_filename, simplify = FALSE)
+#parseData in DataFrame
+tweets.df <- parseTweets(tw_filename, simplify = FALSE)
 #delete JSON file
 file.remove(tw_filename)
 #write in CSV
 tw_filename <- paste(c("Data/", tw_track, "_", current.time, ".csv"), collapse = "")
-write.csv2(tweets, file = tw_filename)
+write.csv2(tweets.df, file = tw_filename)
 

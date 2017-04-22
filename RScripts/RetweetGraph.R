48a468605fb5a3d9e9361aba02c2a6277057613b
macalityy <marcel.wittich@gmail.com>
2017-04-22 17:30:42 +0200

    Finished Script to transform CSV File with tweet data into Retweet<->Tweet Graph Object and save it to RData File

--- RScripts/RetweetGraph.R
+++ RScripts/RetweetGraph.R
@@ -13,10 +13,40 @@ file <- unlist(args[1])
 tweets.df <- read.csv2(file = file, header = TRUE, stringsAsFactors = FALSE)
 
 # from the data frame, we only need the column text to find out which rows are retweets
-tweets_text <- sapply(tweets.df, function(x) x$getText())
+tweets_text <- tweets.df[,"text"]
 
-# now we need to identify retweets
-# they can be identified by finding the pattern: "RT<whitespace>@<username>"
-rt_id("(RT)(?:\b\W@\w+)",tweets_text, ignore.case = TRUE, value = TRUE)
+#not necessary
+#retweets_texts <- grep("(RT)(?:\\b\\W@\\w+)", tweets_text, ignore.case = TRUE, value = TRUE)
 
+# now we need all retweet ids (those lines in tweets.df which are actual retweets)
+retweets_ids  <- grep("(RT)(?:\\b\\W@\\w+)", tweets_text, ignore.case = TRUE )
+
+# create list to store user names of retweets
+user_originaltw <- as.list( 1:length(retweets_ids))
+user_retweet <- as.list(1:length(retweets_ids))
+
+
+for (i in 1:length(retweets_ids))
+{
+  #get tweet text
+  tweet_text <- tweets_text[[retweets_ids[i]]]
+  #find user who posted original tweet
+  tweet_original_poster <- str_extract_all(tweet_text, "(RT)(?:\\b\\W@\\w+)" )
+  
+  #save user who posted original tweet in vector
+  user_originaltw[[i]] = gsub("(RT @)", "", tweet_original_poster, ignore.case = TRUE )
+  #save user who retweeted in vector
+  user_retweet [[i]] = tweets.df[retweets_ids[i],"screen_name"]
+                 
+}
+
+user_originaltw <- unlist(user_originaltw)
+user_retweet <- unlist(user_retweet)
+
+# generate edgelist
+edgelist <- cbind(user_retweet, user_originaltw)
+
+rt_graph = graph.edgelist(edgelist)
+
+save(rt_graph, file = paste(c(file, "_graph.RData")))
 
